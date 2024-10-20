7793d71bf1d09ff753aa0123ff7298e32545e7e4
liamthecoolguy2341 <126356004+liamthecoolguy2341@users.noreply.github.com>
2024-10-20 15:39:03 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main{
+  public static void main(String[] args) {
+    JFrame f = new JFrame();
+    f.setTitle("frame");
+    f.setsize(300,300);
+    f.setvisible(true);
+  }
+}
